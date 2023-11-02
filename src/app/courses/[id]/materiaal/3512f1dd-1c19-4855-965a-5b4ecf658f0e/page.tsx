0e84c1758352ebe1357c1072806536bb50999f9d
Mimi Magusin <mimi_magusin@hotmail.com>
2023-11-02 15:29:42 +0100

    Fix build errors

--- src/app/courses/[id]/materiaal/3512f1dd-1c19-4855-965a-5b4ecf658f0e/page.tsx
+++ src/app/courses/[id]/materiaal/3512f1dd-1c19-4855-965a-5b4ecf658f0e/page.tsx
@@ -5,7 +5,7 @@ export default function CoursePage({ params }: { params: { id: string}}) {
 
   return (
     <div>
-      <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSZI54gWBUEsD3hEl9uZcDgLKWvuk0BaxssxYwloIkIbpOByyMiJrrkc01O6OZGs4w4sgzgSSrsA356/embed?start=false&loop=false&delayms=600000" frameborder="0" width="1000" height="600" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+      <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSZI54gWBUEsD3hEl9uZcDgLKWvuk0BaxssxYwloIkIbpOByyMiJrrkc01O6OZGs4w4sgzgSSrsA356/embed?start=false&loop=false&delayms=600000" width="1000" height="600" allowFullScreen={true}></iframe>
     </div>
   )
 }

3ad3b29be1d0e4bd21114b933dd42a6a3a515aee
Mimi Magusin <mimi_magusin@hotmail.com>
2025-07-04 14:13:24 +0200

    update dates and add new courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,12 @@ import Link from "next/link";
 const ActionsHeroHome = () => (
   <section className="mt-10 flex items-center justify-center gap-x-6">
     <Link
+      href={`/courses/zomerzangdagen`}
+      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      Zomerzangdagen
+    </Link>
+    <Link
       href={`/courses`}
       className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
